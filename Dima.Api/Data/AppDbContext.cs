c7aed461fb33fc4f5f001f7656e71bd666406da0
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-08-20 22:09:12 -0300

    EF e migrations

---
+++ Dima.Api/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Dima.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
