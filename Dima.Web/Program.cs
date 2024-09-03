92a0c0aab2b916fd26011780da36e0d230d5bcc1
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-09-03 15:27:39 -0300

    secao de identity com cookie finalizada

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -2,13 +2,32 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
 using MudBlazor.Services;
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
+using Dima.Core.Handlers;
+using Dima.Web.Handlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackenUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
+{
+    opt.BaseAddress = new Uri(Configuration.BackenUrl);
+}).AddHttpMessageHandler<CookieHandler>();
+
+builder.Services.AddTransient<IAccountHandler, AccountHandler>();
 
 await builder.Build().RunAsync();
